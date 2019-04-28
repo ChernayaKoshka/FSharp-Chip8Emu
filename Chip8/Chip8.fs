8063c456e2ae3156347759630abd9b8b189740ad
FatherFoxxy <rattusunison@gmail.com>
2019-04-27 22:41:36 -0700

    Add a true main function

--- Chip8/Chip8.fs
+++ Chip8/Chip8.fs
@@ -1,10 +1,11 @@
 module Chip8
-open System
-open System.IO
 open Decoder
 
 [<EntryPoint>]
 let main argv =
-    test()
+    if argv.Length = 1 then
+        runFile false argv.[0]
+    elif argv.Length = 2 then
+        runFile true argv.[0]
     printfn "%A" argv
     0 // return an integer exit code
